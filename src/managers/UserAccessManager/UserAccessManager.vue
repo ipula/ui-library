2c46f4146a5d7c937eb1b0817db875272bc51e0c
ipula <ipularanasinghe007@gmail.com>
2024-10-14 18:01:17 +0200

    add user search function

--- src/managers/UserAccessManager/UserAccessManager.vue
+++ src/managers/UserAccessManager/UserAccessManager.vue
@@ -3,15 +3,15 @@
 		<template #label>
 			<h3 class="text-3xl-bold">
 				{{ t('grid.user.currentUsers') }}({{
-					store.invitationsPagination.itemCount
+					store.userAccessPagination.itemCount
 				}})
 			</h3>
 		</template>
 		<template #top-controls>
-			<div class="flex space-x-0">
+			<div class="inline-flex space-x-0">
 				<Icon
 					icon="search"
-					class="border-x-2 border-y-2 p-3"
+					class="border border-r-0 border-light p-3"
 					:inline="true"
 				></Icon>
 				<FieldText
@@ -33,30 +33,30 @@
 			</TableColumn>
 		</TableHeader>
 		<TableBody>
-			<TableRow v-for="(invitation, index) in store.invitations" :key="index">
+			<TableRow v-for="(user, index) in store.userList" :key="index">
 				<TableCell>
-					{{ invitation.fullName }}
-					<Icon v-if="invitation.orcidIsVerified" icon="orcid" :inline="true" />
+					{{ user.fullName }}
+					<Icon v-if="user.orcidIsVerified" icon="orcid" :inline="true" />
 				</TableCell>
 				<TableCell>
-					{{ invitation.email }}
+					{{ user.email }}
 				</TableCell>
 				<TableCell>
-					<template v-for="(userGroups, i) in invitation.groups" :key="i">
+					<template v-for="(userGroups, i) in user.groups" :key="i">
 						<div class="flex flex-col">
 							{{ localize(userGroups.name) }}
 						</div>
 					</template>
 				</TableCell>
 				<TableCell>
-					<template v-for="(userGroups, i) in invitation.groups" :key="i">
+					<template v-for="(userGroups, i) in user.groups" :key="i">
 						<div class="flex flex-col">
 							{{ formatShortDate(userGroups?.startDate) }}
 						</div>
 					</template>
 				</TableCell>
 				<TableCell>
-					{{ localize(invitation.affiliation) }}
+					{{ localize(user.affiliation) }}
 				</TableCell>
 				<TableCell>
 					<DropdownActions
@@ -98,8 +98,7 @@
 						:display-as-ellipsis="true"
 						direction="left"
 						@action="
-							(actionName) =>
-								store.handleInvitationAction(actionName, invitation)
+							(actionName) => store.handleUserAccessAction(actionName, user)
 						"
 					/>
 				</TableCell>
@@ -108,9 +107,9 @@
 	</PkpTable>
 	<div class="flex justify-end">
 		<Pagination
-			:current-page="store.invitationsPagination.currentPage"
-			:last-page="store.invitationsPagination.pageCount"
-			:is-loading="store.isInvitationLoading"
+			:current-page="store.userAccessPagination.currentPage"
+			:last-page="store.userAccessPagination.pageCount"
+			:is-loading="store.isUserAccessLoading"
 			:show-adjacent-pages="3"
 			@set-page="store.setCurrentPage"
 		/>
@@ -132,6 +131,7 @@ import {useDate} from '@/composables/useDate';
 import DropdownActions from '@/components/DropdownActions/DropdownActions.vue';
 import FieldText from '@/components/Form/fields/FieldText.vue';
 import {ref} from 'vue';
+
 const store = useUserAccessManagerStore();
 const {t} = useTranslation();
 const {formatShortDate} = useDate();
